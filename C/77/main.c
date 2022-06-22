c2e137784f1a6f0fec88278c664c0b287de2259f
NanoAnubis <lubomir_stoykov@abv.bg>
2022-06-22 11:40:18 -0400

    Added empty 77

---
+++ C/77/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<err.h>
+#include<time.h>
+
+
+
+int main (int argc, cahr* argv[]) {
+	
+	int log = open("foo.log", O_WRONLY);
+	
+	
+
+
+
+	exit(0);
+}
+
+
