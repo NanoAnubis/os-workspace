dbfa69c0e1835d44ee69b36d060d5aa91dd3fd9b
NanoAnubis <lubomir_stoykov@abv.bg>
2022-06-23 06:19:12 -0400

    Added main.c and makefile template

---
+++ C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+int main (int argc, char* argv[]) {
+
+
+	exit(0);
+}
