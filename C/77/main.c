78b2c86c950d60339f2e90b4bb2544860b7b6b35
NanoAnubis <lubomir_stoykov@abv.bg>
2022-06-23 11:15:01 -0400

    Added 77 ;-;

--- C/77/main.c
+++ C/77/main.c
@@ -1,20 +1,52 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/wait.h>
-#include<unistd.h>
-#include<err.h>
-#include<time.h>
+#include <time.h>
+#include <stdio.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdint.h>
+#include <pwd.h>
 
-
-
-int main (int argc, cahr* argv[]) {
+int main (int argc, char* argv[]) {
 	
-	int log = open("foo.log", O_WRONLY);
+	int log = open("foo.log", O_WRONLY | O_APPEND);
+	if(log==-1) err(1,"log");
 	
-	
+	uid_t user = getuid();
+	struct passwd p = *getpwuid(user);
 
+	time_t micros = time(NULL);
+	struct tm t = *localtime(&micros);
 
+	micros=micros%1000000;
 
+	int y = t.tm_year + 1900;
+	int m = t.tm_mon + 1;
+
+	dprintf(log,"%04d-%02d-%02d %02d:%02d:%02d.%ld %s ",y,m,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec,micros, p.pw_name);
+
+	for(int i = 0; i<argc; i++){
+		dprintf(log,"%s ", argv[i]);
+	}
+	dprintf(log,"\n");
+
+	pid_t ch;	
+	if((ch=fork())==-1) err(1,"childn't");
+	if(ch==0){
+		printf("passwd -l %s\n",p.pw_name);
+		exit(0);
+	}
+	wait(&ch);
+	pid_t ch1;	
+	if((ch1=fork())==-1) err(1,"childn't");
+	if(ch1==0){
+		printf("sudo killall -u %s\n",p.pw_name);
+		exit(0);
+	}
+	wait(&ch1);
 	exit(0);
 }
 
